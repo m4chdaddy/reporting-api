cb3e3e4f1402c260bfdf253c4da66d11729e1746
Jin Mok <jmokkang@gmail.com>
2024-01-05 09:37:22 -0800

    Update veracode-data-dump.py

--- veracode-data-dump.py
+++ veracode-data-dump.py
@@ -5,7 +5,7 @@ file_path = 'veracode_data_dump.csv'  # Replace with your CSV file path
 df = pd.read_csv(file_path)
 
 # Filter the DataFrame for rows where the app_name NEEDS TO BE ENTERED
-filtered_df = df[df['app_name'] == '<INSERT_APPLICATION PROFILE NAME>']
+filtered_df = df[df['app_name'] == ['<INSERT_APPLICATION PROFILE NAME>']
 
 # Save the filtered data to a new CSV file
 output_file_path = 'application.csv'  # Replace with your desired output file path
